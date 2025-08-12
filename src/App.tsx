8293e76906d2a1c4ff27100c7af6d18daa3a95dc
TedDPig_git <85619008+TedDPig123@users.noreply.github.com>
2025-08-12 13:14:37 -0400

    updated metadata

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import Navbar from "./components/Navbar"
 function App() {
   return (
     <>
+      <meta name="description" content="Description"></meta>
+      <meta name="keywords" content="Geri, Gerindra, Geri Adi, Gerindra Adi, Computer science, Software engineer, Software, Artificial Intelligence, Artificial, Undergraduate, Web Development, Website, Software Development, Hardware, Research, Backend, Frontend, Internship, Intern, Student, Indonesia, United States, UMass Amherst, UMass, Massachusetts, Technology, Engineer, Design, CSS, React, Figma, C"></meta>
+      <title>Gerindra Adi - Programmer, Artist, among other things</title>
       <section>
         <Navbar />
         <Landing />
